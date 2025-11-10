0e15785153dbd25e12a721fc00c168cba11aaa97
DemirMasic <demir.masic@stu.ibu.edu.ba>
2025-11-10 16:20:26 +0100

    fixed column names in the DB so they can work properly with Basedao

--- webfall25_db.sql
+++ webfall25_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2025 at 03:16 PM
+-- Generation Time: Nov 10, 2025 at 04:19 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `web25fall_db`
+-- Database: `webfall25_db`
 --
 
 -- --------------------------------------------------------
@@ -39,7 +39,7 @@ CREATE TABLE `favorites` (
 --
 
 CREATE TABLE `image` (
-  `image_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `listing_id` int(11) NOT NULL,
   `image_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -51,7 +51,7 @@ CREATE TABLE `image` (
 --
 
 CREATE TABLE `listing` (
-  `listing_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `brand` varchar(50) NOT NULL,
   `model` varchar(50) NOT NULL,
@@ -73,7 +73,7 @@ CREATE TABLE `listing` (
 --
 
 CREATE TABLE `message` (
-  `message_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `listing_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `content` text NOT NULL,
@@ -87,7 +87,7 @@ CREATE TABLE `message` (
 --
 
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -98,7 +98,7 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `email`, `password`, `created_at`) VALUES
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `created_at`) VALUES
 (1, 'demir', 'demir.masic@stu.ibu.edu.ba', 'testpassword', '2025-11-05 10:37:55'),
 (3, 'sinan', 'sinan.sakic@stu.ibu.edu.ba', 'testpassword', '2025-11-05 10:38:52');
 
@@ -117,21 +117,21 @@ ALTER TABLE `favorites`
 -- Indexes for table `image`
 --
 ALTER TABLE `image`
-  ADD PRIMARY KEY (`image_id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_image_listing` (`listing_id`);
 
 --
 -- Indexes for table `listing`
 --
 ALTER TABLE `listing`
-  ADD PRIMARY KEY (`listing_id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_listing_user` (`user_id`);
 
 --
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
-  ADD PRIMARY KEY (`message_id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_message_listing` (`listing_id`),
   ADD KEY `fk_message_user` (`user_id`);
 
@@ -139,7 +139,7 @@ ALTER TABLE `message`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -149,25 +149,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `image`
 --
 ALTER TABLE `image`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `listing`
 --
 ALTER TABLE `listing`
-  MODIFY `listing_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -177,27 +177,27 @@ ALTER TABLE `user`
 -- Constraints for table `favorites`
 --
 ALTER TABLE `favorites`
-  ADD CONSTRAINT `fk_favorites_listing` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`listing_id`),
-  ADD CONSTRAINT `fk_favorites_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `fk_favorites_listing` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`id`),
+  ADD CONSTRAINT `fk_favorites_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `image`
 --
 ALTER TABLE `image`
-  ADD CONSTRAINT `fk_image_listing` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`listing_id`);
+  ADD CONSTRAINT `fk_image_listing` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`id`);
 
 --
 -- Constraints for table `listing`
 --
 ALTER TABLE `listing`
-  ADD CONSTRAINT `fk_listing_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `fk_listing_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `message`
 --
 ALTER TABLE `message`
-  ADD CONSTRAINT `fk_message_listing` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`listing_id`),
-  ADD CONSTRAINT `fk_message_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `fk_message_listing` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`id`),
+  ADD CONSTRAINT `fk_message_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
