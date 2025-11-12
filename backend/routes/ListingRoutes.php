3df57ce182d878e2f0147be2b750da057ea2375a
DemirMasic <demir.masic@stu.ibu.edu.ba>
2025-11-12 10:31:47 +0100

    started working on publish listing service

--- backend/routes/ListingRoutes.php
+++ backend/routes/ListingRoutes.php
@@ -9,6 +9,10 @@ Flight::route('GET /brandmodel', function(){
    $model = Flight::request()->query['model'] ?? null;
    Flight::json(Flight::listingService()->getByBrandAndModel($brand, $model));
 });
+Flight::route('POST /add_listing', function(){
+   $data = Flight::request()->data->getData();
+   Flight::json(Flight::listingService()->create($data));
+});
 
 
 ?>
